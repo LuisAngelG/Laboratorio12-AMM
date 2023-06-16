874de2637155e4c8d036b4ea2ef00779789bc650
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2023-06-16 16:22:24 -0500

    primer examen

---
+++ Laboratorio12/Laboratorio12/ModelViews/Operacion.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Laboratorio12.ModelViews
+{
+    internal class Operacion: ViewModelBase
+    {
+        //NUMERO 1 QUE RECEPCIONA VALORES
+        int numeroBase;
+
+        public int NumeroBase
+        {
+            get { return numeroBase; }
+            set
+            {
+
+                if (numeroBase != value)
+                {
+                    numeroBase = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        //NUMERO 2 QUE RECEPCIONA VALORES
+        int numeroAltura;
+
+        public int NumeroAltura
+        {
+            get { return numeroAltura; }
+            set
+            {
+
+                if (numeroAltura != value)
+                {
+                    numeroAltura = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        //RESULTADO DE LA OPERACION AREA
+        public ICommand Area { get; set; }
+
+        int resultadoArea;
+
+        public int ResultadoArea
+        {
+            get { return resultadoArea; }
+            set
+            {
+
+                if (resultadoArea != value)
+                {
+                    resultadoArea = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        //RESULTADO DE LA OPERACION PERIMETRO
+        public ICommand Perimetro { get; set; }
+        
+        int resultadoPerimetro;
+
+        public int ResultadoPerimetro
+        {
+            get { return resultadoPerimetro; }
+            set
+            {
+
+                if (resultadoPerimetro != value)
+                {
+                    resultadoPerimetro = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Operacion()
+        {
+            Area = new Command(() =>
+            {
+                ResultadoArea = NumeroAltura * NumeroBase;
+            });
+
+            Perimetro = new Command(() =>
+            {
+                ResultadoPerimetro = (2*NumeroAltura) + (2* NumeroBase);
+            });
+        }
+    }
+}
---
+++ Laboratorio12/Laboratorio12/Views/CalcularMedidas.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio12.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcularMedidas : ContentPage
+    {
+        public CalcularMedidas()
+        {
+            InitializeComponent();
+            this.BindingContext = new ModelViews.Operacion();
+        }
+    }
+}
